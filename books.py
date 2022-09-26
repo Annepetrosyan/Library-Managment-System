87367172fdb6ef4d2f535398181816009b105d2a
Annepetrosyan <anpetrosyan1998@gmail.com>
2022-09-26 18:01:37 +0400

    4-th commit

--- books.py
+++ books.py
@@ -1,4 +1,5 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from datetime import datetime
 from enum import Enum
 
 class BookCategory(Enum):
@@ -23,10 +24,55 @@ class Address:
     self.__country = country
 
 class Person(ABC):
-  def __init__(self, name, address, email, phone):
+  def __init__(self, name, address, email, phone, idx):
     self.__name = name
     self.__address = address
     self.__email = email
     self.__phone = phone
+    self.__idx = idx
 
+
+class Librarian(Person):
+  def __init__(self, name, adress, email, phone, idx):
+    super().__init__(name, adress, email, phone, idx)
+
+  def add_book_item(self, book_item):
+    None
+
+  def block_member(self, member):
+    None
+
+  def un_block_member(self, member):
+    None
+
+
+
+class Member(Person):
+  def __init__(self, name, adress, email, phone, idx):
+    super().__init__(name, adress, email, phone, idx)
+    self.__date_of_membership = datetime.date.today()
+    self.__total_books_checkedout = 0
+
+  def get_total_books_checkedout(self):
+    return self.__total_books_checkedout
+
+  def reserve_book_item(self, book_item):
+    pass
+
+  def increment_total_books_checkedout(self):
+    pass
+
+  def renew_book_item(self, book_item):
+    pass
+
+class Book(ABC):
+  def __init__(self, ISBN, title, subject, publisher, language, number_of_pages, quantity):
+    self.__ISBN = ISBN
+    self.__title = title
+    self.__category = BookCategory.value
+    self.__publisher = publisher
+    self.__language = language
+    self.__number_of_pages = number_of_pages
+    self.__authors = []
+    self.__quantity = quantity
 
