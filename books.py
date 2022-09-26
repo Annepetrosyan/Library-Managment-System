746249ce61b98a1a5c537541a6dfa609251bb150
Annepetrosyan <anpetrosyan1998@gmail.com>
2022-09-26 17:41:05 +0400

    third commit

--- books.py
+++ books.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from enum import Enum
 
 class BookCategory(Enum):
@@ -8,5 +9,24 @@ class BookStatus(Enum):
 
 class ReservationStatus(Enum):
   WAITING, PENDING, CANCELED, NONE = 1, 2, 3, 4
+
+class Constants:
+  MAX_BOOKS_ISSUED_TO_A_USER = 5
+  MAX_LENDING_DAYS = 14
+
+class Address:
+  def __init__(self, street, city, state, zip_code, country):
+    self.__street_address = street
+    self.__city = city
+    self.__state = state
+    self.__zip_code = zip_code
+    self.__country = country
+
+class Person(ABC):
+  def __init__(self, name, address, email, phone):
+    self.__name = name
+    self.__address = address
+    self.__email = email
+    self.__phone = phone
 
 
